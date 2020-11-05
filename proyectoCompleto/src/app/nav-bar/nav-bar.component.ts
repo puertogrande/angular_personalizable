e1cd1daafbb438cce77ec2773914fd778d84b424
manuel <manuelpuertogutierrez@.com>
2020-11-05 10:50:30 +0100

    cambio texto

--- proyectoCompleto/src/app/nav-bar/nav-bar.component.ts
+++ proyectoCompleto/src/app/nav-bar/nav-bar.component.ts
@@ -6,12 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent implements OnInit {
-  
+  nombreEnlace1: string;
+  nombreEnlace2: string;
+  nombreEnlace3: string;
+  nombreEnlace4: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.nombreEnlace1 = "cambio1";
+    this.nombreEnlace2 = "cambio2";
+    this.nombreEnlace3 = "cambio3";
+    this.nombreEnlace4 = "cambio4";
+   }
    
+   changeEnlace(nuevoNombre: string) {
+    this.nombreEnlace1 = nuevoNombre;
+  }
+  //seleccionamos los nombres de los enlaces por defecto
+  ngOnInit(): void {
+    this.nombreEnlace1 = "cambio1";
+    this.nombreEnlace2 = "cambio2";
+    this.nombreEnlace3 = "cambio3";
+    this.nombreEnlace4 = "cambio4";
   }
 
 }
