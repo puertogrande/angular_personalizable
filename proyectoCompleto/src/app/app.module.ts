c92d8487a5b86caed0af784cfd1bf8790eee8745
manuel <manuelpuertogutierrez@.com>
2020-11-13 14:20:36 +0100

    introducion de moldes

--- proyectoCompleto/src/app/app.module.ts
+++ proyectoCompleto/src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { InicioContenidoComponent } from './inicio-contenido/inicio-contenido.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    InicioContenidoComponent
+    InicioContenidoComponent,
+  
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,    //added here too
+    ReactiveFormsModule, //added here too
   ],
   providers: [],
   bootstrap: [AppComponent]
--- proyectoCompleto/src/app/inicio-contenido/inicio-contenido.component.ts
+++ proyectoCompleto/src/app/inicio-contenido/inicio-contenido.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import{ GlobalVars} from '../common/globalVars';
 
 
@@ -8,51 +9,34 @@ import{ GlobalVars} from '../common/globalVars';
   styleUrls: ['./inicio-contenido.component.css']
 })
 export class InicioContenidoComponent implements OnInit {
-  //seria mas recomendable crear una clase solo con los valores de titulo y descripcion
   modo:boolean = GlobalVars.modoCreacion;
-  moldes:Molde[]=[];
-  nuevoMolde:Molde = new Molde();  
-  molde1:Molde = new Molde();
-  molde2:Molde = new Molde();
+  moldeFormGroup: FormGroup;
+  todosmoldes:Molde[];
+  private nuevoMolde: Molde = new Molde();
 
-  constructor() {
-    this.moldes = []; 
-    this.molde1.setTitulo("molde1ssssssssssss");
-    this.molde1.setDescripcion("Esta es la descripción del modulo 1");
-    this.molde2.setTitulo("molde2");
-    this.molde2.setDescripcion("Esta es la descripción del modulo 2");
-    this.moldes.push(this.molde1);
-    this.moldes.push(this.molde2);
-    console.log("estos son los moldes");
-    console.log(this.moldes);
+  constructor(private fb: FormBuilder) {
+    this.todosmoldes=[];
+    this.moldeFormGroup = this.fb.group({
+      titulo: new FormControl(''),
+      descripcion: new FormControl('')
+    });
    }
   
   ngOnInit(): void {
-  
-   
   }
-  crearMolde(){
-     
-    //introducimos el objeto en la lista y lo reinicializamos
-    //moldes.push(nuevoMolde);
-    //this.nuevoMolde = new(Molde);
-
+  addMolde() {
+    console.log('submit');
+    this.nuevoMolde = <Molde> this.moldeFormGroup.value;
+    //console.log(this.newBook instanceof Book);
+    console.log(this.nuevoMolde);
+    this.todosmoldes.push(this.nuevoMolde);
+    console.log("Estos son todos los moldes");
+    console.log(this.todosmoldes);
+    this.nuevoMolde = new Molde();
   }
-  
 }
 
 class Molde {
-  titulo: String;
-  descripcion: String;
-
-  constructor() {
-  
-  }
-  setTitulo(titulo:String){
-    this.titulo = titulo;
-  }
-  setDescripcion(descripcion:String){
-    this.descripcion = descripcion;
-  }
-
+  public titulo: string;
+  public descripcion: string;
 }
